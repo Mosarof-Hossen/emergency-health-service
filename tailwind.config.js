19bb1968221466b045b5d9658cb363009c0af97d
Mosarof-Hossen <mmd58553@gmail.com>
2025-08-27 08:20:46 +0600

    navbar section added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+  module.exports = {
+      theme: {
+        extend: {
+          fontFamily: {
+            
+            hindMadurai: ['"Hind Madurai"', 'sans-serif'],
+            body: ['"Lato"', 'sans-serif'],
+          },
+        },
+      },
+      plugins: [],
+    };
