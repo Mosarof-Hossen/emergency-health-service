690ba80254b025bf65a9996a01b3ee5d6138aaf9
Mosarof-Hossen <mmd58553@gmail.com>
2026-03-08 10:59:35 +0600

    calling button click Each call will cut 20 coins. Reduce Coin after each click

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-
-let heartCount=0
+let heartCount = 0;
 let coinCount = 100;
 let copyCount = 0;
 
-const wishList=(icon)=>{
- 
-     icon.classList.toggle("text-red-800");
+const wishList = (icon) => {
+  icon.classList.toggle("text-red-800");
 
   if (icon.classList.contains("text-red-800")) {
     heartCount++;
   } else {
     heartCount--;
   }
-    document.getElementById("love").innerText=heartCount;
-    
-}
+  document.getElementById("love").innerText = heartCount;
+};
 
 // copy button click function
-const copyBtn=(number)=>{
-    navigator.clipboard.writeText(number)
-    copyCount++;
-    document.getElementById("copy-btt").innerText=copyCount;
-    alert("Number count copy :"+ number)
+const copyBtn = (number) => {
+  navigator.clipboard.writeText(number);
+  copyCount++;
+  document.getElementById("copy-btt").innerText = copyCount;
+  alert("Number count copy :" + number);
+};
+// call button click function
+const callHistory = [];
 
+const callNowBtn = (name, number) => {
+  if (coinCount < 20) {
+    alert("Not enough coins to make a call!");
+    return;
+  }
+  coinCount-=20;
+  document.getElementById("coin-count").innerText=coinCount;
+  alert(`calling ${name} ${number}`)
+  callHistory.push({name,number})
+};
+
+
+// call history function
+
+const showHistory=()=>{
 
 }
-// call button click function
-const callHistory=[];
